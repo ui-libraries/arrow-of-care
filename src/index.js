7ded596067db1fb51810a5f3073f1888f85a251d
Matthew Butler <matthewtbutler@gmail.com>
2020-09-29 16:54:37 -0500

    responsive containers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import logoImg from "./assets/rocket.png"
 import startButtonImg from "./assets/start-button.png"
 import leaderboardButtonImg from "./assets/leaderboard-button.png"
 import gameboardImg from "./assets/background.jpg"
+import characterCardImg from "./assets/character-card.png"
 
 const screenWidth = window.innerWidth
 const screenHeight = window.innerHeight
@@ -22,9 +23,16 @@ class UIScene extends Phaser.Scene {
     const logo = this.add.image(screenWidth/2, screenHeight/2 - 50, "logo").setScale(0.2)
     const startButton = this.add.sprite(logo.x - 30, logo.y + (logo.displayHeight/2 + 40), "start-button").setScale(0.2).setInteractive()
     const leaderboardButton = this.add.sprite(logo.x - 30, startButton.y + (startButton.displayHeight/2 + 10), "leaderboard-button").setScale(0.2).setInteractive()
-    startButton.on('pointerdown', function () {
+    startButton.on('pointerdown', () => {
       this.scene.start('mainScene')
-  }, this)
+    }, this)
+  }
+}
+
+class Character {
+  constructor (name, health) {
+    this.name = name
+    this.health = health   
   }
 }
 
@@ -35,10 +43,22 @@ class mainScene extends Phaser.Scene {
 
   preload () {
     this.load.image("gameboard", gameboardImg)
+    this.load.image("characterCard", characterCardImg )
   }
 
   create () {
     const gameboard = this.add.image(screenWidth/2, screenHeight/2, "gameboard").setScale(1.1)
+    const card1Data = new Character("matthew", 20)
+    const card1Container = this.add.container(200, screenHeight - 200).setScale(0.25)
+    
+    console.log(card1Container.width)
+    const card1Image = this.add.image(0,0, "characterCard")
+    const card1ContainerWidth = card1Image.width * 0.25
+    console.log(card1Image.width)
+    const card1Text = this.add.text((0 - card1Image.width/2) + 40, (0 - card1Image.height/2 + 60), card1Data.name, {fontSize:120, color: "white"})
+    card1Container.add(card1Image)
+    card1Container.add(card1Text)
+    
   }
 }
 
