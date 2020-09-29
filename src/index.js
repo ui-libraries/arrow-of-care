282a4d7cde8af4b3a4fbf745c10f45b6d43e7f9f
Matthew Butler <matthewtbutler@gmail.com>
2020-09-29 11:53:54 -0500

    add logo and centered button images

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from "phaser"
+import logoImg from "./assets/rocket.png"
+import startButtonImg from "./assets/start-button.png"
+import leaderboardButtonImg from "./assets/leaderboard-button.png"
+
+const screenWidth = window.innerWidth
+const screenHeight = window.innerHeight
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: screenWidth,
+  height: screenHeight,
   scene: {
     preload: preload,
     create: create
   }
-};
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image("logo", logoImg)
+  this.load.image("start-button", startButtonImg)
+  this.load.image("leaderboard-button", leaderboardButtonImg)
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  
+  const logo = this.add.image(screenWidth/2, screenHeight/2 - 50, "logo").setScale(0.2)
+  console.log(screenHeight/2, logo)
+  const startButton = this.add.image(logo.x - 30, logo.y + (logo.displayHeight/2 + 40), "start-button").setScale(0.2)
+  const leaderboardButton = this.add.image(logo.x - 30, startButton.y + (startButton.displayHeight/2 + 10), "leaderboard-button").setScale(0.2)
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
 }
