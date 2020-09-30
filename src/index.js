67349826daecad887d8dbc9da5fe727469eaf905
Ethan DeGross <ethan-degross@uiowa.edu>
2020-09-30 12:14:28 -0500

    Character class
    Working container class extension

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import leaderboardButtonImg from "./assets/leaderboard-button.png"
 import gameboardImg from "./assets/background.jpg"
 import characterCardImg from "./assets/character-card.png"
 
-const screenWidth = window.innerWidth
+const screenWidth = window.innerWidth 
 const screenHeight = window.innerHeight
+const column = screenWidth/12
+const row = screenHeight/12
 
+console.log(window.devicePixelRatio/3)
 class UIScene extends Phaser.Scene {
   constructor () {
     super({ key: 'UIScene' });
@@ -20,7 +23,7 @@ class UIScene extends Phaser.Scene {
   }
 
   create () {
-    const logo = this.add.image(screenWidth/2, screenHeight/2 - 50, "logo").setScale(0.2)
+    const logo = this.add.image(screenWidth/2, screenHeight/2 - 50, "logo").setScale(.20)
     const startButton = this.add.sprite(logo.x - 30, logo.y + (logo.displayHeight/2 + 40), "start-button").setScale(0.2).setInteractive()
     const leaderboardButton = this.add.sprite(logo.x - 30, startButton.y + (startButton.displayHeight/2 + 10), "leaderboard-button").setScale(0.2).setInteractive()
     startButton.on('pointerdown', () => {
@@ -29,10 +32,23 @@ class UIScene extends Phaser.Scene {
   }
 }
 
-class Character {
-  constructor (name, health) {
-    this.name = name
-    this.health = health   
+class Character extends Phaser.GameObjects.Container {
+  constructor(scene, x, y, key1, text) {
+      super(scene)
+
+      this.scene = scene
+      this.x = x
+      this.y = y
+
+      const card = this.scene.add.image(x,y, key1)
+      console.log(card)
+      const cardText = this.scene.add.text((0 - card.width/4) + 40, (0 - card.height/4 + 60), text, {fontSize:120, color: "white"})
+
+      this.add(card)
+      this.add(cardText)
+
+      this.scene.add.existing(this)
+      this.setScale(0.25)
   }
 }
 
@@ -47,18 +63,8 @@ class mainScene extends Phaser.Scene {
   }
 
   create () {
-    const gameboard = this.add.image(screenWidth/2, screenHeight/2, "gameboard").setScale(1.1)
-    const card1Data = new Character("matthew", 20)
-    const card1Container = this.add.container(200, screenHeight - 200).setScale(0.25)
-    
-    console.log(card1Container.width)
-    const card1Image = this.add.image(0,0, "characterCard")
-    const card1ContainerWidth = card1Image.width * 0.25
-    console.log(card1Image.width)
-    const card1Text = this.add.text((0 - card1Image.width/2) + 40, (0 - card1Image.height/2 + 60), card1Data.name, {fontSize:120, color: "white"})
-    card1Container.add(card1Image)
-    card1Container.add(card1Text)
-    
+    const gameboard = this.add.image(screenWidth/2, screenHeight/2, "gameboard").setScale(.75)//need to stretch scale to screen width
+    const card = new Character(this, screenWidth * .1, screenHeight * .6, "characterCard", "matthew")
   }
 }
 
