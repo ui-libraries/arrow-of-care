8744af9523fb939c9b6dd76d4d1d42f86c29623c
Ethan DeGross <ethan-degross@uiowa.edu>
2020-10-13 14:33:54 -0500

    Update cards.js

--- src/cards.js
+++ src/cards.js
@@ -3,6 +3,7 @@ export const cards = {
             "text": "Dr. Baby has a high fever.",
             "skill": "H",
             "targets": ["baby"],
+            "numTargets": 2,
             "value": -2,
             "type": "care"
         },
@@ -10,40 +11,456 @@ export const cards = {
             "text": "Robert has lice and needs his person/possessions cleaned.",
             "skill": "H",
             "targets": ["robert"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Rosario is feeling despair about whether the rocket can be completed in tme.",
+            "skill": "E",
+            "targets": ["rosario"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Blaise mourns a breakup.",
+            "skill": "E",
+            "targets": ["blaise"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Tammy is having an anxiety attack.",
+            "skill": "E",
+            "targets": ["tammy"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Tammy has an infection in her leg.",
+            "skill": "H",
+            "targets": ["tammy"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Robert is having arthritic pain in his hands.",
+            "skill": "L",
+            "targets": ["robert"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Blaise has sprained their ankle.",
+            "skill": "L",
+            "targets": ["blaise"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Robert has cardiac arrest and needs CPR.",
+            "skill": "H",
+            "targets": ["robert"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Keara chokes on food and needs the Heimlich maneuver administered.",
+            "skill": "H",
+            "targets": ["keara"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Keara's memory is failing and she is confused.",
+            "skill": "E",
+            "targets": ["keara"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Yusef has a bad dream.",
+            "skill": "E",
+            "targets": ["yusef"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Yusef wanders off and gets lost.",
+            "skill": "L",
+            "targets": ["yusef"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Blaise has an ear infection.",
+            "skill": "L",
+            "targets": ["blaise"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Dr. Baby is constipated.",
+            "skill": "L",
+            "targets": ["baby"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Dinner needs to be cooked.",
+            "skill": "L",
+            "targets": [""],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Food needs to be found in the wreckage.",
+            "skill": "H",
+            "targets": [""],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Bathroom facilities must be built.",
+            "skill": "H",
+            "targets": [""],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Rosario has sliced her arm.",
+            "skill": "L",
+            "targets": ["rosario"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Tammy has a cold.",
+            "skill": "L",
+            "targets": ["tammy"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Robert has food poisoning.",
+            "skill": "L",
+            "targets": ["robert"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Rosario has a migraine.",
+            "skill": "L",
+            "targets": ["rosario"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Maya has sprained her wrist.",
+            "skill": "L",
+            "targets": ["maya"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Maya's fibromyalgia acts up.",
+            "skill": "L",
+            "targets": ["maya"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Dr. Baby has diaper rash.",
+            "skill": "L",
+            "targets": ["baby"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Keara needs medicine administered.",
+            "skill": "L",
+            "targets": ["keara"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Dr. Baby is not getting to sleep easily and needs to be held to go to sleep.",
+            "skill": "L",
+            "targets": ["baby"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Blaise has an allergic reaction.",
+            "skill": "L",
+            "targets": ["blaise"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Rosario has an asthma attack.",
+            "skill": "L",
+            "targets": ["rosario"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Clothing need to be cleaned.",
+            "skill": "L",
+            "targets": [],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Robert misses his children and is depressed.",
+            "skill": "E",
+            "targets": ["robert"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Yusef is bored.",
+            "skill": "E",
+            "targets": ["yusef"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Tammy has a nightmare.",
+            "skill": "E",
+            "targets": ["yusef"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Maya is emotionally exhausted",
+            "skill": "E",
+            "targets": ["maya"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Yusef is having a meltdown.",
+            "skill": "E",
+            "targets": ["yusef"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Maya is feeling depressed that her husband is now a five-year old.",
+            "skill": "E",
+            "targets": ["maya"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Housing needs to be built.",
+            "skill": "H",
+            "targets": [],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Keara has fallen and fractured a hip.",
+            "skill": "H",
+            "targets": ["keara"],
+            "numTargets": 2,
+            "value": -2,
+            "type": "care"
+        },
+        {
+            "text": "Rocket needs to be scrubbed down.",
+            "skill": "H",
+            "targets": [],
+            "numTargets": 2,
             "value": -2,
             "type": "care"
         }
+
     ],
     bonus: [{
             "text": "Extra food found.",
             "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
             "value": 1,
             "type": "bonus"
         },
         {
             "text": "Medicine found",
             "targets": ["baby", "yusef", "keara"],
+            "numTargets": 3,
             "value": 1,
             "type": "bonus"
             },
         {
             "text": "Rocket owner's manual discovered.",
-            "targets": "rocket",
+            "targets": ["rocket"],
+            "numTargets": 1,
             "value": 1,
             "type": "bonus"
-        }
+        },
+        {
+            "text": "Fresh spring discovered.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": 1,
+            "type": "bonus"
+        },
+        {
+            "text": "Pasising satellite delivers cell service for an hour.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": 1,
+            "type": "bonus"
+        },
+        {
+            "text": "Save this card and use it to get out of the crevasse.",
+            "targets": "crevasse-bonus",
+            "numTargets": 1,
+            "value": 1,
+            "type": "bonus"
+        },
+        {
+            "text": "First aid kit found.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": 1,
+            "type": "bonus"
+        },
+        {
+            "text": "Housing materials found.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": 1,
+            "type": "bonus"
+        },
+        {
+            "text": "Everyone gets quality sleep.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": 1,
+            "type": "bonus"
+        },
+        {
+            "text": "Extra tools found to work on ship.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": 1,
+            "type": "bonus"
+        },
+
     ],
     catastrophe: [{
             "text": "Chemical leaks into water source.",
             "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
             "value": -2,
             "type": "catastrophe"
         },
         {
-            "text": "Housing needs to be built.",
-            "targets": ["assign"],
-            "value": -2,
+            "text": "Space creatures surround the camp and need to be scared off.",
+            "targets": ["blaise", "robert", "rosario"],
+            "numTargets": 3,
+            "value": -1,
             "type": "catastrophe"
+        },
+        {
+            "text": "Space bugs eat produce.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -1,
+            "type": "catastrophe"
+        },
+        {
+            "text": "Housing is destroyed by space debris.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -2,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Space illness.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -2,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Generator fails and everyone catches a cold.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -2,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Those working on rocket are burned.",
+            "targets": ["blaise", "robert", "rosario"],
+            "numTargets": 3,
+            "value": -3,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Dust storm delays build.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -1,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Player of your choice gets seriously ill.",
+            "targets": [],
+            "numTargets": 1,
+            "value": -2,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Robert burns dinner.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -1,
+            "type": "catastrophe" 
+        },
+        {
+            "text": "Space debris destorys part ot the ship.",
+            "targets": ["blaise", "robert", "rosario", "baby", "keara", "maya", "tammy", "yusef"],
+            "numTargets": 8,
+            "value": -2,
+            "type": "catastrophe" 
         }
     ],
     start: [
